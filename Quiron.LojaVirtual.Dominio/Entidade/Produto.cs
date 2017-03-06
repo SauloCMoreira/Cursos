de98b5fb7f2e1e70db1e3318de72dcbc92705ac0
SauloCMoreira <saulocmoreira@hotmail.com>
2017-03-06 10:51:28 -0300

    Clasee Produto

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -13,5 +13,6 @@ namespace Quiron.LojaVirtual.Dominio.Entidade
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
+
     }
 }
